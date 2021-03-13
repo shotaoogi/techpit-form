4d12f99f091c6a263ec1dc53c10928d149d6db3d
shotaoogi <shota.oogi@gmail.com>
2021-03-13 21:55:26 +0900

    Reduxにaddressを追加

--- path/to/app/front/src/domain/entity/profile.ts
+++ path/to/app/front/src/domain/entity/profile.ts
@@ -1,8 +1,10 @@
 import { Gender } from "./gender";
+import { Address } from './address'
 
 export type Profile = {
   name: String;
   description: string;
   birthday: string;
   gender: Gender;
+  address: Address;
 }
