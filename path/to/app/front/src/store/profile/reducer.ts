0f803c02b2b1c239303639be9e17e8a85421f169
shotaoogi <shota.oogi@gmail.com>
2021-03-15 14:16:14 +0900

    Redux の action を追加

--- path/to/app/front/src/store/profile/reducer.ts
+++ path/to/app/front/src/store/profile/reducer.ts
@@ -14,7 +14,12 @@ const init: Profile = {
     city: "",
     restAddress: ""
   },
-  careers: []
+  careers: [],
+  college: {
+    name: '',
+    faculty: '',
+    department: ''
+  }
 };
 
 const initCareer: Career = {
@@ -50,6 +55,10 @@ const profileReducer = reducerWithInitialState(init)
   .case(profileActions.addCareer, state => ({
     ...state,
     careers: [...state.careers, initCareer]
+  }))
+  .case(profileActions.setCollege, (state, payload) => ({
+    ...state,
+    college: { ...state.college, ...payload }
   }));
 
 export default profileReducer
